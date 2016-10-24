23137400f91dc15a03a3da8464c1c2fa5c4a417a
mengtingdiandian <335764940@qq.com>
2016-10-24 20:39:12 +0800

    add

--- Web/AddPage/AddClient.aspx.cs
+++ Web/AddPage/AddClient.aspx.cs
@@ -31,88 +31,87 @@ public partial class Addpage_AddClient : System.Web.UI.Page
         string userName ="";
         string passwd ="";
         if(string.IsNullOrEmpty(this.Class.Text)){
-             classname = "null";
+             classname = "";
         }else{
              classname = this.Class.Text;
         }
 
          if(string.IsNullOrEmpty(this.Serial.Text)){
-             serial = "null";
+             serial = "";
         }else{
              serial = this.Serial.Text;
         }
 
          if(string.IsNullOrEmpty(this.Client.Text)){
-             client = "null";
+             client = "";
         }else{
              client = this.Client.Text;
         }
 
          if(string.IsNullOrEmpty(this.Tel.Text)){
-            tel = "null";
+            tel = "";
         }else{
             tel = this.Tel.Text;      
         }
 
          if(string.IsNullOrEmpty(this.Tel2.Text)){
-            tel2 = "null";
+            tel2 = "";
         }else{
             tel2 = this.Tel2.Text;
         }
 
          if(string.IsNullOrEmpty(this.Country.Text)){
-            country = "null";
+            country = "";
         }else{
              country = this.Country.Text;        
         }
 
          if(string.IsNullOrEmpty(this.Province.Text)){
-            province = "null";
+            province = "";
         }else{
             province = this.Province.Text;
         }
 
          if(string.IsNullOrEmpty(this.City.Text)){
-            city = "null";
+            city = "";
         }else{
             city = this.City.Text;
         }
        
         if(string.IsNullOrEmpty(this.Addr.Text)){
-            addr = "null";
+            addr = "";
         }else{
             addr = this.Addr.Text;     
         }
 
          if(string.IsNullOrEmpty(this.Email.Text)){
-            email = "null";
+            email = "";
         }else{
             email = this.Email.Text;
         }
 
          if(string.IsNullOrEmpty(this.UserName.Text)){
-            userName = "null";
+            userName = "";
         }else{
             userName = this.UserName.Text;
         }
 
         if(string.IsNullOrEmpty(this.Passwd.Text)){
-            passwd = "null";
+            passwd = "";
         }else{
              passwd = this.Passwd.Text;
         }
 
         dal.AddClient(classname,serial,client,tel,tel2,country,province,city,addr,email,userName,passwd);
-
-
         //if (a > 0)
         //{
-           Response.Redirect("DBTableManagement.aspx");
+           System.Threading.Thread.Sleep(1000);
+           Response.Redirect("../DBTableManagement.aspx");
            Response.End();
         // }
         // else
         //{
-        //        System.Threading.Thread.Sleep(2000);
+        //        
         //        Response.Redirect("AddClient.aspx");
         //        Response.End();
         // }
--- Web/UpdatePage/UpdateClient.aspx.cs
+++ Web/UpdatePage/UpdateClient.aspx.cs
@@ -14,12 +14,50 @@ public partial class UpdatePage_UpdateClient : System.Web.UI.Page
         string id = Request.QueryString["selectclientid"];
         ClientDal dal = new ClientDal();
         DataSet ds = dal.SelectById(id);
+        DataTable dt = ds.Tables[0];
+        this.Class.Text = dt.Columns[1].ToString();
+        this.Serial.Text = dt.Columns[2].ToString();
+        this.Client.Text = dt.Columns[3].ToString();
+        this.Tel.Text =  dt.Columns[4].ToString();
+        this.Tel2.Text =  dt.Columns[5].ToString();
+        this.Country.Text = dt.Columns[6].ToString();
+        this.Province.Text =  dt.Columns[7].ToString();
+        this.City.Text =  dt.Columns[8].ToString();
+        this.Addr.Text =  dt.Columns[9].ToString();
+        this.Email.Text = dt.Columns[10].ToString();
+        this.UserName.Text =  dt.Columns[11].ToString();
+        this.Passwd.Text = dt.Columns[12].ToString();
+        //this.Class.Text = ds.Tables[0].Columns[1].ToString();
+        //this.Serial.Text = ds.Tables[0].Columns[2].ToString();
+        //this.Client.Text = ds.Tables[0].Columns[3].ToString();
+        //this.Tel.Text = ds.Tables[0].Columns[4].ToString();
+        //this.Tel2.Text = ds.Tables[0].Columns[5].ToString();
+        //this.Country.Text = ds.Tables[0].Columns[6].ToString();
+        //this.Province.Text = ds.Tables[0].Columns[7].ToString();
+        //this.City.Text = ds.Tables[0].Columns[8].ToString();
+        //this.Addr.Text = ds.Tables[0].Columns[9].ToString();
+        //this.Email.Text = ds.Tables[0].Columns[10].ToString();
+        //this.UserName.Text = ds.Tables[0].Columns[11].ToString();
+        //this.Passwd.Text = ds.Tables[0].Columns[12].ToString();
+        //this.Class.Text = ds.Tables[0].Columns[1][0];
+        //this.Serial.Text = ds.Tables[0].Columns[2][0];
+        //this.Client.Text = ds.Tables[0].Columns[3][0];
+        //this.Tel.Text = ds.Tables[0].Columns[4][0];
+        //this.Tel2.Text = ds.Tables[0].Columns[5][0];
+        //this.Country.Text = ds.Tables[0].Columns[6][0];
+        //this.Province.Text = ds.Tables[0].Columns[7][0];
+        //this.City.Text = ds.Tables[0].Columns[8][0];
+        //this.Addr.Text = ds.Tables[0].Columns[9][0];
+        //this.Email.Text = ds.Tables[0].Columns[10][0];
+        //this.UserName.Text = ds.Tables[0].Columns[11][0];
+        //this.Passwd.Text = ds.Tables[0].Columns[12][0];
 
     }
 
     #region   提交按钮
     protected void Submit_Click(object sender, EventArgs e)
     {
+        string id = Request.QueryString["selectclientid"];
         ClientDal dal = new ClientDal();
         string classname = "";
         string serial = "";
@@ -35,7 +73,7 @@ public partial class UpdatePage_UpdateClient : System.Web.UI.Page
         string passwd = "";
         if (string.IsNullOrEmpty(this.Class.Text))
         {
-            classname = "null";
+            classname = "";
         }
         else
         {
@@ -44,7 +82,7 @@ public partial class UpdatePage_UpdateClient : System.Web.UI.Page
 
         if (string.IsNullOrEmpty(this.Serial.Text))
         {
-            serial = "null";
+            serial = "";
         }
         else
         {
@@ -53,7 +91,7 @@ public partial class UpdatePage_UpdateClient : System.Web.UI.Page
 
         if (string.IsNullOrEmpty(this.Client.Text))
         {
-            client = "null";
+            client = "";
         }
         else
         {
@@ -62,7 +100,7 @@ public partial class UpdatePage_UpdateClient : System.Web.UI.Page
 
         if (string.IsNullOrEmpty(this.Tel.Text))
         {
-            tel = "null";
+            tel = "";
         }
         else
         {
@@ -71,7 +109,7 @@ public partial class UpdatePage_UpdateClient : System.Web.UI.Page
 
         if (string.IsNullOrEmpty(this.Tel2.Text))
         {
-            tel2 = "null";
+            tel2 = "";
         }
         else
         {
@@ -80,7 +118,7 @@ public partial class UpdatePage_UpdateClient : System.Web.UI.Page
 
         if (string.IsNullOrEmpty(this.Country.Text))
         {
-            country = "null";
+            country = "";
         }
         else
         {
@@ -89,7 +127,7 @@ public partial class UpdatePage_UpdateClient : System.Web.UI.Page
 
         if (string.IsNullOrEmpty(this.Province.Text))
         {
-            province = "null";
+            province = "";
         }
         else
         {
@@ -98,7 +136,7 @@ public partial class UpdatePage_UpdateClient : System.Web.UI.Page
 
         if (string.IsNullOrEmpty(this.City.Text))
         {
-            city = "null";
+            city = "";
         }
         else
         {
@@ -107,7 +145,7 @@ public partial class UpdatePage_UpdateClient : System.Web.UI.Page
 
         if (string.IsNullOrEmpty(this.Addr.Text))
         {
-            addr = "null";
+            addr = "";
         }
         else
         {
@@ -116,7 +154,7 @@ public partial class UpdatePage_UpdateClient : System.Web.UI.Page
 
         if (string.IsNullOrEmpty(this.Email.Text))
         {
-            email = "null";
+            email = "";
         }
         else
         {
@@ -125,7 +163,7 @@ public partial class UpdatePage_UpdateClient : System.Web.UI.Page
 
         if (string.IsNullOrEmpty(this.UserName.Text))
         {
-            userName = "null";
+            userName = "";
         }
         else
         {
@@ -134,19 +172,19 @@ public partial class UpdatePage_UpdateClient : System.Web.UI.Page
 
         if (string.IsNullOrEmpty(this.Passwd.Text))
         {
-            passwd = "null";
+            passwd = "";
         }
         else
         {
             passwd = this.Passwd.Text;
         }
 
-        dal.UpdateClientById(classname, serial, client, tel, tel2, country, province, city, addr, email, userName, passwd);
+        dal.UpdateClientById(classname, serial, client, tel, tel2, country, province, city, addr, email, userName, passwd,id);
 
 
         //if (a > 0)
         //{
-        Response.Redirect("DBTableManagement.aspx");
+        Response.Redirect("../DBTableManagement.aspx");
         Response.End();
         // }
         // else
