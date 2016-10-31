30ca4cc64326b9cc91759319208b037494e8836a
Administrator <Administrator@iZvch0jedulrpkZ>
2016-10-31 12:11:05 +0800

    动态添加表格

---
+++ Web/Scripts/common.js
@@ -0,0 +1,224 @@
+﻿$(function () {
+    //头部链接状态
+    var currUrl = location.pathname;
+    $('#nav-tabs a').each(function (i, e) {
+        if ($(this).attr('href').indexOf(currUrl) == 0) {
+            $(this).parents().siblings().removeClass('edited');
+            $(this).parent().addClass('edited');
+            $('#nav-tabs li:lt(' + i + ')').addClass('finished');
+        }
+    });
+
+    $(".number").live('keyup', function () {
+        onlyNumber(this);
+    });
+
+    $(".detepickers").live("focus", function () {
+        $(this).datepicker({
+            dateFormat: "yy-mm-dd",
+            //showOn: "button", 
+            //buttonImage: "images/calendar.png",
+            //buttonImageOnly: true,
+            changeYear: true,
+            changeMonth: true,
+            onSelect: function () {
+                $(this).removeClass("hasDatepicker");
+                $(this).attr("id", "");
+
+            },
+            onClose: function () {
+                var errorMsg = "";
+                $(this).attr("id", "");
+                $(this).removeClass("hasDatepicker");
+
+                if ($.trim($(this).val()) == "") {
+                    errorMsg = "日期为必填项!";
+                    $(this).addClass('error').after("<span class='help-block text-error'>" + errorMsg + "</span>");
+                }
+                if ($.trim($(this).val()) != "") {
+                    var reg = /^(\d{1,4})(-|\/)(\d{1,2})\2(\d{1,2})$/;
+                    if ($(this).val().match(reg) == null) {
+                        errorMsg = "请输入正确的时间格式!";
+                        $(this).addClass('error').after("<span class='help-block text-error'>" + errorMsg + "</span>");
+                    }
+                }
+            }
+            //  showOn: "both"
+        });
+    });
+
+    //表格中必填项验证
+    $(".required").live('blur', function () {
+        if ($.trim($(this).val()) != "") {
+            return;
+        }
+
+        var errorMsg = "此字段为必填项";
+        switch ($(this).attr("type")) {
+            case "text":
+                if ($(this).attr("errormsg") != undefined) {
+                    errorMsg = $(this).attr("errormsg");
+                }
+                break;
+            case "select":
+                var sval = $(this).find("option:selected").val();
+                if (sval === "-1") {
+                    if ($(this).attr("errormsg") != undefined) {
+                        errorMsg = $(this).attr("errormsg");
+                    }
+                }
+                break;
+            default:
+        }
+
+        $(this).addClass('error').after("<span class='help-block text-error'>" + errorMsg + "</span>");
+
+    });
+
+    $(".error").live('focus', function () {
+        $(this).removeClass('error').parent("td").find(".text-error").fadeOut(1500, function () { $(this).remove(); });
+
+    });
+
+    //非表格中必填项验证
+    $(".requirednt").live('blur', function () {
+        if ($.trim($(this).val()) != "") {
+            return;
+        }
+        var errorMsg = "此字段为必填项";
+        switch ($(this).attr("type")) {
+            case "text":
+                if ($(this).attr("errormsg") != undefined) {
+                    errorMsg = $(this).attr("errormsg");
+                }
+                break;
+            case "select":
+                var sval = $(this).find("option:selected").val();
+                if (sval === "-1") {
+                    if ($(this).attr("errormsg") != undefined) {
+                        errorMsg = $(this).attr("errormsg");
+                    }
+                }
+                break;
+            default:
+        }
+
+        $(this).addClass('errornt').after("<span class='help-block text-error'>" + errorMsg + "</span>");
+
+    });
+
+    $(".errornt").live('focus', function () {
+        $(this).removeClass('errornt').parent().find(".text-error").fadeOut(1500, function () { $(this).remove(); });
+
+    });
+
+    //长度限制
+    $(".maxlength").live('keyup', function () {
+        var max = 0;
+        if ($(this).attr("maxlength") == undefined) {
+            max = 20;
+        } else {
+            max = parseInt($(this).attr("maxlength"));
+        }
+
+        $(this).val($(this).val().substring(0, max));
+
+    });
+
+    //控制复选框单选
+    $(".checkboxs").live('click', function () {
+        var $parent = $(this).parent();
+        $parent.find("[type='checkbox']").each(function () {
+            $(this).removeAttr('checked');
+        });
+        $(this).attr('checked', 'checked');
+    });
+
+    $(".serverCheckbox").live('click', function () {
+        var $parent = $(this).parent().parent().parent();
+        $parent.find("[type='checkbox']").each(function () {
+            $parent.find("[type='checkbox']").each(function () {
+                $(this).removeAttr('checked');
+            });
+            $(this).attr('checked', 'checked');
+        });
+
+        //控制单选框单选
+        $(".radios").live('click', function () {
+            var $parent = $(this).parent();
+
+            $parent.children().each(function () {
+                $(this).removeAttr('checked');
+            });
+            $(this).attr('checked', 'checked');
+        });
+
+        $(".serverRadios").live('click', function () {
+            var $parent = $(this).parent().parent().parent();
+
+            $parent.find("[type='radio']").each(function () {
+                $(this).removeAttr('checked');
+            });
+            $(this).attr('checked', 'checked');
+        });
+    });
+
+    //有无变化|有无此情况
+    $('.radio :radio').live('click', (function () {
+        //radio按钮值
+        var cboxVal = $(this).attr('value');
+        //表格ID
+        var nearTableId = $(this).parents().next('.table').attr('id');
+        if (cboxVal == 0)
+            $(document.getElementById(nearTableId)).find('td').each(function () {
+                $(this).children().attr("disabled", "disabled");
+            });
+        else
+            $(document.getElementById(nearTableId)).find('td').each(function () {
+                $(this).children().removeAttr("disabled");
+            });
+    }));
+
+});
+
+//增加行
+function addTableRow(objId) {
+    $.fn.tables.addRow(objId);
+}
+
+//控制只能输入数字
+function onlyNumber(obj) {
+    if (obj.value.length > 20) {
+        obj.value = obj.value.substring(0, 20);
+    }
+    obj.value = obj.value.replace(/[^\d.]/g, "");
+    obj.value = obj.value.replace(/^\./g, "");
+    obj.value = obj.value.replace(/\.{2,}/g, ".");
+    obj.value = obj.value.replace(".", "$#$").replace(/\.replace/g, "").replace("$#$", ".");
+}
+
+//验证是否为数字
+function checkIsNumber(number) {
+    //TODO:
+}
+
+//验证是否为日期
+function checkIsDate(date) {
+    //TODO:
+}
+
+
+//验证结束时间是否大于开始时间
+function compareDate(startDate, endDate) {
+    //TODO:
+}
+
+//检查是否为空
+function checkIsEmpty() {
+    //TODO:
+}
+
+//验证下拉框是否已选
+function checkSelected() {
+    //TODO:
+}
--- Web/SystemConfig/DictManagement.aspx
+++ Web/SystemConfig/DictManagement.aspx
@@ -6,12 +6,153 @@
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title></title>
+        <script type="text/javascript" src="../Scripts/jquery-1.7.1.min.js"></script>
+    <script type="text/javascript" src="../Scripts/jquery-ui-1.8.20.min.js"></script>
+    <script type="text/javascript" src="../Scripts/jquery-ui-tables.js"></script>
+
 </head>
 <body>
     <form id="form1" runat="server">
     <div>
-    
+        <table class="table table-editable" id="tbMainClass">
+            <thead>
+                <tr>
+                    <th>分类编码：</th>
+                    <th>分类名称：</th>
+                    <th>序号：</th>
+                    <th><a href="javascript:addMailClassTableRow('tbMainClass')" title="新增"><span class="icon icon-add">xinzeng</span></a></th>                      
+                </tr>
+                <tr>
+                    <td><input id="Relation" type="text"  style="width: 100px" class="required" /></td>
+                    <td><input id="RelativesName" type="text" style="width: 100px" class="required" /></td>
+                    <td><input id="UnitName" type="text" style="width: 150px" class="required" /></td>
+                    <td>&nbsp;</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr >
+                    <td>
+                        </td>
+                    <td> 
+                        </td>
+                    <td>
+                        </td>  
+                    <td></td>                 
+                </tr>
+            </tbody>
+        </table>
+        <div class="table-toolbar"></div>
     </div>
+    <div>
+        <table class="table table-editable" id="tbRelativesJob">
+            <thead>
+                <tr>
+                    <th>编码</th>
+                    <th>名称</th>
+                    <th>序号</th>                         
+                    <th><a href="javascript:addTableRow('tbRelativesJob')" title="新增"><span class="icon icon-add">xinzeng</span></a></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr >
+                    <td>
+                        <input name="Relation" type="text" errormsg="必填" style="width: 100px" class="required" /></td>
+                    <td>
+                        <input name="RelativesName" type="text" style="width: 100px" class="required" /></td>
+                    <td>
+                        <input name="UnitName" type="text" style="width: 150px" class="required" /></td>                    
+                    <td>
+                        <button type="button" name="Id" class="btn btn-link" onclick="deleteRow(this)"><span class="icon icon-remove"></span></button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="table-toolbar"></div>
+    </div>
+
+         <script type="text/javascript">
+  
+             var ee = 4;
+             var resultArray = new Array();
+             
+            
+             resultArray.push({ w2: ee, e3: "", r3: "" });
+           
+             resultArray.push( { w2: "555", e3: "", r3: "" });
+           
+        //删除行
+             function deleteRow(obj) {                 
+                $.fn.tables.deleteRow(obj);
+        }
+
+        //增加行
+        function addTableRow(gridId) {
+            $.fn.tables.addRow(gridId);
+        }
+        function ShowValue(value1,value2,value3)
+        {
+            $("#Relation").val(value1);
+            $("#RelativesName").val(value2);
+            $("#UnitName").val(value3);
+        }
+        function addMailClassTableRow(gridId)
+        {
+            $.fn.tables.addRow(gridId);
+            $("#" + gridId).find("tr").last().find("td").each(function (index, item) {
+                if (index == 0)
+                {
+                   
+                    $(this).text($("#Relation").val());
+                }
+                else if (index == 1)
+                {
+                    $(this).text($("#RelativesName").val());
+                }
+                else if (index == 2) {
+                    $(this).text($("#UnitName").val());
+                }
+                else {
+                    $(this).parent().bind("click", function () {
+                       
+                        ShowValue($(this).find("td").eq(0).text(), $(this).find("td").eq(1).text(), $(this).find("td").eq(2).text());
+                    })
+                    $(this).parent().css("cursor", "hand");
+                    $(this).html("<button type=\"button\" name=\"Id\" class=\"btn btn-link\" onclick=\"deleteRow(this)\">删除</button>")
+                }
+                
+            });
+        }
+
+        //校验数据
+
+        function validateRow()
+        {
+            var ispass = $.fn.tables.validateRow('tbRelativesJob');
+           
+            if (!ispass) {
+                layer.alert("还有必填项未填写！请继续完善后再保存!");
+            }
+
+        }
+
+        //取结果
+        function getRowResult()
+        {
+            var relativesJob = $.fn.tables.getResult('tbRelativesJob');
+            $("#resultData").text(relativesJob.join('|'));
+
+            //这种写法也可取到
+            //var rowResult = $("#tbRelativesJob").tables.getResult('tbRelativesJob');
+            //alert(rowResult);
+
+            
+        }
+
+        function bindData()
+        {
+            $.fn.tables.bindData('tbRelativesJob', $("#hdRelatives").val());
+        }
+    </script>
     </form>
 </body>
 </html>
