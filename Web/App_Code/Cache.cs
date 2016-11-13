e9d11175c5b25c4e086aec1739a7186bc69d6e80
kingmile <kingmile@KINGMILE-PC>
2016-11-13 13:14:59 +0800

    contact

--- Web/App_Code/Cache.cs
+++ Web/App_Code/Cache.cs
@@ -11,11 +11,13 @@ using System.Web;
 public class DataCache
 {
     private static List<ModelDictDetail> dict;
-    
+    private static List<ModelSpec> dictSpec;
+
     static DataCache()
     {
         ServiceCommon servComm = new ServiceCommon();
-        dict = servComm.GetListTop<ModelDictDetail>(0,null).ToList();  
+        dict = servComm.GetListTop<ModelDictDetail>(0,null).ToList();
+        dictSpec= servComm.GetListTop<ModelSpec>(0, null).ToList();
     }
 
     public static List<ModelDictDetail> findAllDict()
--- Web/SalesManage/GenerateContract.aspx.cs
+++ Web/SalesManage/GenerateContract.aspx.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using D2012.Common.DbCommon;
+using D2012.Domain.Services;
+using Microsoft.Reporting.WebForms;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,10 +11,95 @@ using System.Web.UI.WebControls;
 
 public partial class SalesManage_GenerateContract : System.Web.UI.Page
 {
+    protected string strAction = "0";
+    protected DataTable OrderDetail = new DataTable() ;
+    ServiceCommon servComm = new ServiceCommon();
+    ConditionComponent ccwhere = new ConditionComponent();
     protected void Page_Load(object sender, EventArgs e)
     {
+        ccwhere.AddComponent("Id", "42", SearchComponent.Equals, SearchPad.NULL);
+        OrderDetail = servComm.GetListTop(0, "ViewSalesDetail", ccwhere);
         if (!IsPostBack)
-        { 
+        {
+           
         }
+        else {
+            strAction = "exportContact";
+        }
+    }
+
+    protected void ExportContact_Click(object sender, EventArgs e)
+    {
+        this.SalesContactViewer.LocalReport.DataSources.Clear();
+        DataTable dtContact  = (new SalesDataSet()).Tables["ContactDataTable"];
+        //dtContact.Columns.Add("Seller");
+        //dtContact.Columns.Add("ContactBH");
+        //dtContact.Columns.Add("SignAddr");
+        //dtContact.Columns.Add("Buyer");
+        //dtContact.Columns.Add("SignDatetime");
+        dtContact.Rows.Add(dtContact.NewRow());
+        dtContact.Rows[0]["Seller"] = this.Seller.Text;
+        dtContact.Rows[0]["ContactBH"] = this.ContactBH.Text;
+        dtContact.Rows[0]["SignAddr"] = this.SignAddr.Text;
+        dtContact.Rows[0]["Buyer"] = this.Buyer.Text;
+        dtContact.Rows[0]["SignDatetime"] = Request["SignDatetime"];
+
+        dtContact.Rows[0]["Quantity"] = this.Quantity.Text;
+        dtContact.Rows[0]["ProvideGoods"] = this.ProvideGoods.Text;
+        dtContact.Rows[0]["FeeCharge"] = this.FeeCharge.Text;
+        dtContact.Rows[0]["CheckStandard"] = this.CheckStandard.Text;
+        dtContact.Rows[0]["Statements"] = this.Statements.Text;
+        dtContact.Rows[0]["Contact"] = this.Contact.Text;
+        dtContact.Rows[0]["Solve"] = this.Solve.Text;
+        dtContact.Rows[0]["SignMethod"] = this.SignMethod.Text;
+        dtContact.Rows[0]["AppointedItem"] = this.AppointedItem.Text;
+        dtContact.Rows[0]["AppointedItem1"] = this.AppointedItem1.Text;
+        dtContact.Rows[0]["Liquor"] = this.Liquor.Text;
+        dtContact.Rows[0]["Classify"] = this.Classify.Text;
+
+        dtContact.Rows[0]["SellerBottom"] = this.SellerBottom.Text;
+        dtContact.Rows[0]["BuyerBottom"] = this.BuyerBottom.Text;
+        dtContact.Rows[0]["SellerAddr"] = this.SellerAddr.Text;
+        dtContact.Rows[0]["BuyerAddr"] = this.BuyerAddr.Text;
+        dtContact.Rows[0]["SellerOperator"] = this.SellerOperator.Text;
+        dtContact.Rows[0]["BuyerOperator"] = this.BuyerOperator.Text;
+        dtContact.Rows[0]["SellerTel"] = this.SellerTel.Text;
+        dtContact.Rows[0]["BuyerTel"] = this.BuyerTel.Text;
+        dtContact.Rows[0]["SellerFax"] = this.SellerFax.Text;
+        dtContact.Rows[0]["BuyerFax"] = this.BuyerFax.Text;
+        dtContact.Rows[0]["SellerBank"] = this.SellerBank.Text;
+        dtContact.Rows[0]["BuyerBank"] = this.BuyerBank.Text;
+        dtContact.Rows[0]["BuyerBank"] = this.BuyerBank.Text;
+        dtContact.Rows[0]["SellerAccount"] = this.SellerAccount.Text;
+        dtContact.Rows[0]["BuyerAccount"] = this.BuyerAccount.Text;
+        dtContact.Rows[0]["SellerPostcodes"] = this.SellerPostcodes.Text;
+        dtContact.Rows[0]["BuyerPostcodes"] = this.BuyerPostcodes.Text;
+
+
+
+        DataTable dtSetail = new DataTable();
+        dtSetail.Columns.Add("ProductName");
+        dtSetail.Columns.Add("Spec");
+        dtSetail.Columns.Add("OrderNo");
+        dtSetail.Columns.Add("Unit");
+        dtSetail.Columns.Add("Qty");
+        dtSetail.Columns.Add("Price");
+        dtSetail.Columns.Add("Amount");
+        for (int i = 0;i< OrderDetail.Rows.Count;i++)
+        {
+            dtSetail.Rows.Add(dtSetail.NewRow());
+            dtSetail.Rows[i]["ProductName"] = OrderDetail.Rows[i]["ProductName"];
+            dtSetail.Rows[i]["Spec"] = OrderDetail.Rows[i]["Spec"];
+            dtSetail.Rows[i]["OrderNo"] = OrderDetail.Rows[i]["OrderNo"];
+            //dtSetail.Rows[i]["Unit"] = OrderDetail.Rows[i]["Unit"];
+            dtSetail.Rows[i]["Qty"] = OrderDetail.Rows[i]["Qty"];
+            //dtSetail.Rows[i]["Price"] = OrderDetail.Rows[i]["Price"];
+            //dtSetail.Rows[i]["Amount"] = OrderDetail.Rows[i]["Amount"];
+        }
+        this.SalesContactViewer.LocalReport.DataSources.Add(new ReportDataSource("DataSet1", dtContact));
+        this.SalesContactViewer.LocalReport.DataSources.Add(new ReportDataSource("DataSet2", dtSetail));
+        this.SalesContactViewer.DataBind();
+        this.SalesContactViewer.LocalReport.Refresh();
+
     }
 }
